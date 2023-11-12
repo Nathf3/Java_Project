eadd6a3ea6f5d8120fb59d36ccfbdf3fb20ff818
Nathf3 <nathan.faget02@gmail.com>
2023-11-12 17:23:38 +0100

    mise a jour

--- Runner/src/Main.java
+++ Runner/src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello world");
+        Group root = new Group();
+        Pane pane = new Pane(root);
+        Scene theScene = new Scene(pane, 600, 400,true);
+        primaryStage.setScene(theScene);
+        primaryStage.show();
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
