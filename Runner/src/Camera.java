5cafbfed0d2a93cf0db0d460d5e2a9d4c537fdb9
Nathf3 <nathan.faget02@gmail.com>
2023-11-23 22:55:59 +0100

    add other class

--- Runner/src/Camera.java
+++ Runner/src/Camera.java
@@ -1,2 +1,20 @@
 public class Camera {
+    int x;
+    int y;
+    public Camera(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    @Override
+    public String toString(){
+        return this.x+","+this.y;
+    }
+
 }
